cf988fb90f3f0c5e21d962b94e23cef6ed2595cd
thpierce <thomas.pierce92@gmail.com>
2015-06-15 18:09:58 -0700

    Exploratory work with Cardboard

---
+++ Assets/Scripts/PlayerMotion.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotion : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (forwardFree()){
+			transform.position += Vector3.forward * Time.deltaTime;	
+		}
+	}
+
+	bool forwardFree(){
+		return !(Physics.Raycast(transform.position, Vector3.forward, 1));
+	}
+}
